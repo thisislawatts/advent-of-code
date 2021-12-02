fc0dfbaa3c594aa20575b7291adcc48f9fa91c7e
Luke Watts <luke@thisis.la>
2021-12-02 20:42:30 +0100

    ✨ solution to day 2, part 2

--- 20211202/src/index.ts
+++ 20211202/src/index.ts
@@ -1,10 +1,17 @@
 import * as fs from 'fs';
 
+interface Instruction {
+  direction: Axis;
+  distance: number
+}
+
+type Axis = 'x' | 'y';
+
 const loadFile = (filepath: string) => fs.readFileSync(filepath, { encoding: 'utf-8' })
   .split('\n')
   .map(str => {
     const [dir, num] = str.split(' ');
-    let direction: 'x' | 'y' = 'x';
+    let direction: Axis = 'x';
     let distance = parseInt(num, 10);
 
     if (['down', 'up'].includes(dir)) {
@@ -15,30 +22,32 @@ const loadFile = (filepath: string) => fs.readFileSync(filepath, { encoding: 'ut
       }
     }
 
-    return { direction, distance }
+    return { direction, distance } as Instruction;
   });
 
 const exampleInput = loadFile('./example.txt')
 const exerciseInput = loadFile('./input.txt');
 
-interface Instruction {
-  direction: 'x' | 'y';
-  distance: number
-}
-
 function calculate(input: Instruction[]): number {
   const vector = {
     x: 0,
     y: 0,
+    aim: 0,
   };
 
   input.map((i: Instruction) => {
-    vector[i.direction] += i.distance;
+    if (i.direction === 'x') {
+      vector[i.direction] += i.distance;
+      vector.y += i.distance * vector.aim
+    }
+
+    if (i.direction === 'y') {
+      vector.aim += i.distance;
+    }
   })
 
-  console.log(vector);
   return vector.x * vector.y;
 }
 
-console.log(calculate(exampleInput) === 150);
+console.log(calculate(exampleInput) === 900);
 console.log(calculate(exerciseInput));
