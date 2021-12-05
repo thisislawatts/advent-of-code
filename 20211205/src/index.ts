52b4f4f1d7708b6f1d57a19a6f4a48861b067cfc
Luke Watts <luke@thisis.la>
2021-12-05 10:11:29 +0100

    ✨ solution to day 5, part 2

--- 20211205/src/index.ts
+++ 20211205/src/index.ts
@@ -12,15 +12,20 @@ const loadFile = (filepath: string) => fs.readFileSync(filepath, { encoding: 'ut
 
 const exampleInput = loadFile('./example.txt')
 
-function calculate(input: any[]): number {
+function markCoordinateAsMatched(coordMap, pointer: string) {
+  coordMap[pointer] = (coordMap[pointer] || 0) + 1
+}
+
+type Coordinate = Array<number>;
+
+function calculate(input: Array<Coordinate[]>): number {
 
   const affectedCoords: any = {};
 
-  input.map(([start,end]) => {
+  input.map(([start, end]) => {
     // Ignore lines which are not
     // vertical OR horizontal
     if (start[0] === end[0] || start[1] === end[1]) {
-      console.log({ start, end });
       const minX = Math.min(start[0], end[0]);
       const maxX = Math.max(start[0], end[0]);
 
@@ -29,21 +34,34 @@ function calculate(input: any[]): number {
 
       for (let x = minX; x <= maxX; x++) {
         for (let y = minY; y <= maxY; y++) {
-          if (!affectedCoords[`${x}:${y}`]) {
-            affectedCoords[`${x}:${y}`] = 1;
-          } else {
-            affectedCoords[`${x}:${y}`]++;
+          markCoordinateAsMatched(affectedCoords, `${x}:${y}`);
+        }
+      }
+    } else {
+      const minX = Math.min(start[0], end[0]);
+      const maxX = Math.max(start[0], end[0]);
+
+      const minY = Math.min(start[1], end[1]);
+      const maxY = Math.max(start[1], end[1]);
+
+      const slope = (end[1] - start[1]) / (end[0] - start[0]);
+
+      markCoordinateAsMatched(affectedCoords, `${end[0]}:${end[1]}`);
+
+      for (let x = minX; x <= maxX; x++) {
+        for (let y = minY; y <= maxY; y++) {
+          if (slope === (end[1] - y) / (end[0] - x)) {
+            markCoordinateAsMatched(affectedCoords, `${x}:${y}`);
           }
         }
       }
+
     }
   });
-
-  console.log({ affectedCoords });
 
   return Object.values(affectedCoords).filter((v: any) => v >= 2).length;
 }
 
 const output = calculate(exampleInput);
-console.log(output, output === 5);
+console.log(output, output === 12);
 console.log(calculate(loadFile('./input.txt')));
