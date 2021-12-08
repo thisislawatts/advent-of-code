9d57c0dcbf1a07019d62315961594fe6ae65c2bb
Luke Watts <luke@thisis.la>
2021-12-08 10:43:19 +0100

    ✨ solution to day 8, part 2

--- 20211208/README.md
+++ 20211208/README.md
@@ -1,6 +1,6 @@
-# Day 7
+# Day 8
 
-https://adventofcode.com/2021/day/7
+https://adventofcode.com/2021/day/8
 
 
 A sketchy solution initialised with `npm init typescript`
--- 20211208/src/index.ts
+++ 20211208/src/index.ts
@@ -4,26 +4,90 @@ const loadFile = (filepath: string) => fs.readFileSync(filepath, { encoding: 'ut
   .split('\n')
   .map(v => {
     const [signal, output] = v.split(' | ')
+    // Noramlize values to be in alphabetical order
     return {
       output: output.split(' ').map(str => str.split('').sort().join('')),
-      signal: signal.split(' '),
-    }
+      signal: signal.split(' ').map(str => str.split('').sort().join('')).sort((a: string, b: string) => a.length > b.length ? 1 : -1),
+    } as SignalAndOuput
   });
 
-function calculate(inputList: any): number {
-  const digitUsage: any = [];
-  
-  inputList.forEach(({ output }) => {
-    output.forEach(element => {
-      digitUsage[element.length] = (digitUsage[element.length] || 0) + 1
+interface SignalAndOuput {
+  output: string[]
+  signal: string[]
+}
+
+function calculate(inputList: SignalAndOuput[]): number {
+  let sum = 0;
+
+  inputList.forEach(({ output, signal }) => {
+    const signalToDigitMap = {};
+    signal.map(sig => {
+      if (sig.length === 2) {
+        signalToDigitMap[sig] = 1
+      }
+
+      if (sig.length === 4) {
+        signalToDigitMap[sig] = 4
+      }
+
+      if (sig.length === 3) {
+        signalToDigitMap[sig] = 7
+      }
+
+
+      if (sig.length === 7) {
+        signalToDigitMap[sig] = 8
+      }
+
+
+      const oneIs = signal.find(str => str.length === 2)
+      const threeIs: any = signal.find((str: string) => str.length === 5 && str.match(new RegExp(`[${oneIs}]`, 'g'))?.length === 2);
+      const fourIs = signal.find(str => str.length === 4);
+
+      if (sig.length === 5) {
+        // 2, ~3, 5
+        if (sig.match(new RegExp(`[${oneIs}]`, 'g'))?.length === 2) {
+          signalToDigitMap[sig] = 3
+          return;
+        }
+        // `Four` matches 2 element with `Two`
+        // `Four` matches 3 elements with `Five`
+        const matches = sig.match(new RegExp(`[${fourIs}]`, 'g'))?.length;
+
+        if (matches === 3) {
+          signalToDigitMap[sig] = 5
+        } else if (matches === 2) {
+          signalToDigitMap[sig] = 2
+        }
+      }
+
+      if (sig.length === 6) {
+        // ~0, ~6, ~9 
+        if (sig.match(new RegExp(`[${oneIs}]`, 'g'))?.length !== 2) {
+          signalToDigitMap[sig] = 6
+          return;
+        }
+
+        // 9 contains 3
+        if (sig.match(new RegExp(`[${threeIs}]`, 'g'))?.length === 5) {
+          signalToDigitMap[sig] = 9
+        } else {
+          signalToDigitMap[sig] = 0
+        }
+      }
     });
+    // Analyse the `signal` input to understand the valuation
+    let readOut = '';
+    output.forEach(element => {
+      readOut += signalToDigitMap[element];
+    });
+
+    sum += parseInt(readOut);
   });
-  
-  console.log({ digitUsage });
-  // console.log(digitUsage['cg'], digitUsage['bcdf'], digitUsage['acf'], digitUsage['abcdefg'])
-  return digitUsage[2] + digitUsage[3] + digitUsage[4] + digitUsage[7];
+
+  return sum;
 }
 
 const result = calculate(loadFile('./example.txt'));
-console.log(result, result === 26);
+console.log(result, result === 61229);
 console.log(calculate(loadFile('./input.txt')));
