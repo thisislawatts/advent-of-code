023b5385066928a39aef4b7a14f8153a5e3c0865
Luke Watts <luke@thisis.la>
2024-12-02 07:54:37 +0100

    20241202 - puzzle 1

---
+++ 2024/02/main.go
@@ -0,0 +1,80 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+)
+
+func fileToReports(file string) [][]int {
+	input, err := os.ReadFile(file)
+	if err != nil {
+		panic(err)
+	}
+
+	lines := strings.Split(string(input), "\n")
+
+	reports := make([][]int, len(lines))
+
+	for i, line := range lines {
+		values := strings.Fields(line)
+		reports[i] = make([]int, len(values))
+		for j, value := range values {
+			reports[i][j], _ = strconv.Atoi(value)
+		}
+	}
+
+	return reports
+}
+
+func isSafe(report []int) bool {
+	verdict := true
+	direction := 1 // 1 = ascending, -1 = descending
+
+	// The results must all be in a consistent order
+	// either ascending or descending
+	if report[0] > report[1] {
+		direction = -1
+	}
+
+	for i := 0; i < len(report)-1; i++ {
+		if report[i] == report[i+1] {
+			verdict = false
+			break
+		}
+
+		if report[i] > report[i+1] {
+			if direction == 1 {
+				verdict = false
+			}
+
+			if report[i]-report[i+1] > 3 {
+				verdict = false
+			}
+		} else {
+			if direction == -1 {
+				verdict = false
+			}
+
+			if report[i+1]-report[i] > 3 {
+				verdict = false
+			}
+		}
+	}
+
+	return verdict
+}
+
+func main() {
+	reports := fileToReports("input.txt")
+
+	safeCount := 0
+	for _, report := range reports {
+		if isSafe(report) {
+			safeCount++
+		}
+	}
+
+	fmt.Printf("Safe Reports: %v\n", safeCount)
+}
---
+++ 2024/02/main_test.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestIsSafe(t *testing.T) {
+	reports := fileToReports("testInput.txt")
+
+	assert.Equal(t, true, isSafe(reports[0]))
+	assert.Equal(t, false, isSafe(reports[1]))
+	assert.Equal(t, false, isSafe(reports[2]))
+	assert.Equal(t, false, isSafe(reports[3]))
+	assert.Equal(t, false, isSafe(reports[4]))
+	assert.Equal(t, true, isSafe(reports[5]))
+}
