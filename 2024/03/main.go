a564de4b2b98f6520544a774f4bc65c4eccba4f9
Luke Watts <luke@thisis.la>
2024-12-04 13:56:38 +0100

    20241203 - puzzle 2

--- 2024/03/main.go
+++ 2024/03/main.go
@@ -8,19 +8,30 @@ import (
 )
 
 func solve(input string) int {
-	// TODO: Solve the problem
 	// Compile the regex
-	re := regexp.MustCompile(`(?i)mul\((\d{1,3}),(\d{1,3})\)`)
+	re := regexp.MustCompile(`(?i)(mul\((\d{1,3}),(\d{1,3})\)|don't\(\)|do\(\))`)
 
 	// Find all matches
 	matches := re.FindAllString(input, -1)
 
 	sum := 0
-
+	isEnabled := true
 	// Print valid instructions
 	fmt.Println("Valid mul instructions:")
+
 	for _, match := range matches {
 		fmt.Println(match)
+		if match == "don't()" {
+			isEnabled = false
+		}
+		if match == "do()" {
+			isEnabled = true
+		}
+
+		if !isEnabled {
+			continue
+		}
+
 		var a int
 		var b int
 		fmt.Fscanf(strings.NewReader(match), "mul(%d,%d)", &a, &b)
--- 2024/03/main_test.go
+++ 2024/03/main_test.go
@@ -8,6 +8,6 @@ import (
 
 func TestSolve(t *testing.T) {
 	// TODO: Add test cases.
-	input := `xmul(2,4)%&mul[3,7]!@^do_not_mul(5,5)+mul(32,64]then(mul(11,8)mul(8,5))`
-	assert.Equal(t, solve(input), 161)
+	input := `xmul(2,4)&mul[3,7]!^don't()_mul(5,5)+mul(32,64](mul(11,8)undo()?mul(8,5))`
+	assert.Equal(t, solve(input), 48)
 }
